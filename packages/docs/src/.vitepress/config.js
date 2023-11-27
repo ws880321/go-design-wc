f27d2c3aefd6e1eb696a41dbbef105f98a9b9a1d
ws880321 <wangshuai@vip.126.com>
2023-11-27 16:45:08 +0800

    doc edit

--- packages/docs/src/.vitepress/config.js
+++ packages/docs/src/.vitepress/config.js
@@ -2,7 +2,7 @@
  * @Author: ws880321 wangshuai@vip.126.com
  * @Date: 2023-11-23 13:30:23
  * @LastEditors: ws880321 wangshuai@vip.126.com
- * @LastEditTime: 2023-11-27 16:42:12
+ * @LastEditTime: 2023-11-27 16:44:36
  * @FilePath: /go-design/packages/docs/src/.vitepress/config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,7 +12,7 @@ import sidebar from "./config/sidebar";
 import nav from "./config/nav";
 
 export default defineConfig({
-  base: 'go-design-wc',
+  base: '/go-design-wc/',
   title: 'go-design',
   description: '使用 vue3 开发的web component组件库',
   themeConfig: {
